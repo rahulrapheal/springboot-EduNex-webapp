1eed37d9b87375ac09c9f773f70265e2ac90d494
rahulrapheal <145462205+rahulrapheal@users.noreply.github.com>
2026-01-11 23:13:14 +0530

    Delete src directory

--- src/main/java/com/luminar/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.luminar.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.luminar.entity.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
